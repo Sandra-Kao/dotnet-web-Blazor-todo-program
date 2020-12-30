cab4921e58acd081adcd7095993ebe2dad6305b8
Sandra-Kao <63223781+Sandra-Kao@users.noreply.github.com>
2020-12-30 14:06:32 +0800

    Add files via upload
    A fun pratice for .Net Blazor

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TodoItems_Crud_Blazor.Services;
+
+namespace TodoItems_Crud_Blazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            //builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri("https://skyerp.aks.study4.dev/"));
+            
+            builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri("https://todoitemsapiappservice.azurewebsites.net/"));
+            builder.Services.AddSingleton<ITodoItemsService, TodoItemsService>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
